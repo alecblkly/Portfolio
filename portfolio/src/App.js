cd21ae8c954d8ddb4f435059db19d25bcc4aff49
Alec Blakeley <alecblkly@gmail.com>
2020-06-02 16:16:51 -0400

    Removed HTML5 template, updated README for updates, ran CRA for base social media and updating message.

---
+++ portfolio/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+   return (
+      <div className="Container">
+         <div className="Terminal">
+            <div className="Base-Information">
+               <h2 className="Title">Alec Blakeley</h2>
+               <h3 className="Title">Full-Stack Web Developer.</h3>
+               <h4 className="Title">Portfolio is currently being updated.</h4>
+            </div>
+            <div className="Contact-Information">
+               <p className="Social-Contact">[Twitter]</p>
+               <p className="Social-Contact">[LinkedIn]</p>
+               <p className="Social-Contact">[GitHub]</p>
+            </div>
+         </div>
+      </div>
+   );
+}
+
+export default App;
